468e28799e88046ca91782d8dae9e7869d97d055
Sam W <rev2010onyt@gmail.com>
2021-03-04 17:02:23 -0600

    Shutdown command and error logging

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let embed = new Discord.MessageEmbed();
 
 client.on('ready', ()=>{
 	embed.setTimestamp(Date.now());
-	
+
 	embed.setTitle('Online');
 	embed.setDescription('Rhea is online');
 	embed.setColor('4CE31E');
@@ -23,8 +23,25 @@ client.on('message', msg =>{
 	let args = msg.content.substring(PREFIX.length).split(' ');
 
 	if(msg.content.substring(0, 1) === '!') {
-		Commands.cmd(msg, args);
+		switch(args[0]) {
+			case 'shutdown':
+				console.log(`Shutdown in progress... Started by ${msg.member.displayName}`);
+				if(msg.member.roles.cache.has(Settings.role_administrator)) {
+					setTimeout(function() {
+						client.destroy();
+						console.log(`Shutdown complete... Have a nice day`);
+					}, 10000)
+				}
+				break;
+			default:
+				Commands.cmd(msg, args);
+				break;
+		}
 	}
+})
+
+client.on('error', error =>{
+	console.log(`ERROR: ${error.message}`);
 })
 
 client.login(token);
